b979921620354bee50090913de92bf44a51d4771
Karan <karanrajmehta06@gmail.com>
2023-11-27 19:54:26 +0530

    Update reset.sql

--- db/reset.sql
+++ db/reset.sql
@@ -84,27 +84,71 @@ DROP INDEX IF EXISTS follow_request_source_user_id_target_user_id_key;
 -- Drop functions
 DROP FUNCTION IF EXISTS create_user;
 
+DROP FUNCTION IF EXISTS update_user;
+
+DROP FUNCTION IF EXISTS update_user;
+
+DROP FUNCTION IF EXISTS delete_user;
+
 DROP FUNCTION IF EXISTS make_follow_request;
 
 DROP FUNCTION IF EXISTS view_pending_follow_requests;
 
 DROP FUNCTION IF EXISTS accept_follow_requests;
 
+DROP FUNCTION IF EXISTS reject_follow_requests;
+
 DROP FUNCTION IF EXISTS create_post;
+
+DROP FUNCTION IF EXISTS update_post;
+
+DROP FUNCTION IF EXISTS update_post;
+
+DROP FUNCTION IF EXISTS delete_post;
 
 DROP FUNCTION IF EXISTS like_post;
 
+DROP FUNCTION IF EXISTS unlike_post;
+
 DROP FUNCTION IF EXISTS comment_on_post;
+
+DROP FUNCTION IF EXISTS update_comment_on_post;
+
+DROP FUNCTION IF EXISTS update_comment_on_post;
+
+DROP FUNCTION IF EXISTS delete_comment_on_post;
 
 DROP FUNCTION IF EXISTS message_friend;
 
+DROP FUNCTION IF EXISTS update_message_friend;
+
+DROP FUNCTION IF EXISTS update_message_friend;
+
+DROP FUNCTION IF EXISTS view_messages;
+
+DROP FUNCTION IF EXISTS delete_message_friend;
+
 DROP FUNCTION IF EXISTS create_user_group;
 
+DROP FUNCTION IF EXISTS update_user_group;
+
+DROP FUNCTION IF EXISTS update_user_group;
+
+DROP FUNCTION IF EXISTS delete_user_group;
+
 DROP FUNCTION IF EXISTS message_in_group;
+
+DROP FUNCTION IF EXISTS update_message_in_group;
+
+DROP FUNCTION IF EXISTS update_message_in_group;
+
+DROP FUNCTION IF EXISTS delete_message_in_group;
 
 DROP FUNCTION IF EXISTS view_group_messages;
 
 DROP FUNCTION IF EXISTS add_members_to_group;
+
+DROP FUNCTION IF EXISTS remove_members_from_group;
 
 DROP FUNCTION IF EXISTS view_user_posts;
 
@@ -113,6 +157,8 @@ DROP FUNCTION IF EXISTS view_followers;
 DROP FUNCTION IF EXISTS view_posts_of_other_user;
 
 DROP FUNCTION IF EXISTS who_to_follow;
+
+DROP FUNCTION IF EXISTS view_post_feed;
 
 -- Drop triggers
 DROP TRIGGER IF EXISTS update_post_count_trigger ON post;
